89c509fea755760da1bfb76f3566bcf7a3c556fb
Rewale <komaroff.sania2015@gmail.com>
2022-08-22 15:01:10 +0500

    config

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/labstack/gommon/log"
+	"github.com/spf13/viper"
 	todo "go-todo"
 	"go-todo/pkg/handler"
 	"go-todo/pkg/repository"
@@ -9,11 +10,21 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error while reading config: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error: %s", err.Error())
 	}
+}
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
